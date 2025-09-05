09425d9a39678151dc2fcf59463e32d32fb8c0fa
Carpioo <carpioblake@gmail.com>
2025-09-05 16:34:11 -0600

    Se agregan las funciones crear persona, crear la poblacion y la reproduccion donde si tenemos valores con decimales, se tendra la probabilidad de redondear hacia arriba o hacia abajo

--- gen.py
+++ gen.py
@@ -1,15 +1,20 @@
 import random
 
 
-def crar_persona():
+def crear_persona():
     return [random.randint(1,9) for _ in range(20)]
 
-def crar_poblacion(n=100):
-    return [crear_individuo() for _ in range (n)]
+def crear_poblacion(n=100):
+    return [crear_persona() for _ in range (n)]
 
 def reproducir(padre1, padre2):
     hijo1, hijo2 = [], []
     for g1, g2 in zip(padre1, padre2):
         promedio = (g1 + g2) /2
-        if promedio.is_integerer():
+        if promedio.is_integer():
             valor = int(promedio)
+        else:
+            valor = int(promedio) + (1 if random.random()< 0.5 else 0)
+        hijo1.append(valor)
+        hijo2.append(valor)
+    return hijo1, hijo2
